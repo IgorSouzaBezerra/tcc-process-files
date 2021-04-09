7d3c013d772f05d433cbf3541c081dea55431a37
IgorSouzaBezerra <igor.souza1.bezerra@gmail.com>
2021-04-09 18:18:58 -0300

    Add url in log

--- ProcessFile.API/Job/JobEMail.cs
+++ ProcessFile.API/Job/JobEMail.cs
@@ -7,9 +7,9 @@ namespace ProcessFile.API.Job
     {
         public static void Execute(string url)
         {
-            Console.WriteLine("[JOB] Executando - " + DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss"));
+            Console.WriteLine("[JOB] Executando - " + DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss") + " - " + url);
             JobReadingEmailService.Reading(url);
-            Console.WriteLine("[JOB] Finalizando - " + DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss"));
+            Console.WriteLine("[JOB] Finalizando - " + DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss") + " - " + url);
         }
     }
 }
