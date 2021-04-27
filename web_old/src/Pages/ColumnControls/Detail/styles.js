d8a01953ec318d059f5cd172cf541fe5d718cbb4
IgorSouzaBezerra <igor.souza1.bezerra@gmail.com>
2021-04-26 21:24:08 -0300

    remove old project web

--- web_old/src/Pages/ColumnControls/Detail/styles.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export const Content = styled.div`
-    max-width: 1100px;
-    margin: 0 auto;
-
-    text-align: center;
-    margin-top: 50px;
-`;
--- web_old/src/Pages/User/UserDetail/validation.js
+++
@@ -1,7 +0,0 @@
-import * as yup from 'yup';
-
-export const schemaUser = yup.object().shape({
-    name: yup.string().required("O nome é obrigatório."),
-    email: yup.string().email().required("O e-mail é obrigatório.").email("O e-mail informado não é válido"),
-    password: yup.string().required("A senha é obrigatória.").min(10, "Mínimo de 10 caracteres"),
-});
